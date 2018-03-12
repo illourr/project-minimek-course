127f959e7eda9b498769561b9bbe11656b9beaed
Dillon Curry <dilloncurry@gmail.com>
2018-03-12 07:29:36 -0400

    Loading sample data into redux orm pilots models and rendering them.

--- src/features/pilots/Pilots.js
+++ src/features/pilots/Pilots.js
@@ -1,54 +1,60 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-import {
-    Grid,
-    Segment,
-    Header,
-} from "semantic-ui-react";
+import { Grid, Segment, Header } from "semantic-ui-react";
 
+import orm from "app/orm";
 
 import PilotsList from "./PilotsList";
 import PilotDetails from "./PilotDetails";
 
-const pilots = [
-    {
-        name : "Natasha Kerensky",
-        rank : "Captain",
-        age : 52,
-        gunnery : 2,
-        piloting : 3,
-        mechType : "WHM-6R",
-    }
-];
+const mapState = state => {
+  // Create a Redux-ORM Session from our "entities" slice, which
+  // contains the "tables" for each model type
+  const session = orm.session(state.entities);
 
-class Pilots extends Component {
-    state = {
-        pilots : pilots,
-    }
+  // Retrieve the model class that we need.  Each Session
+  // specifically "binds" model classes to itself, so that
+  // updates to model instances are applied to that session.
+  // These "bound classes" are available as fields in the session.
+  const { Pilot } = session;
 
-    render() {
-        const {pilots} = this.state;
+  // Query the session for all Pilot instances.
+  // The QuerySet that is returned from all() can be used to
+  // retrieve instances of the Pilot class, or retrieve the
+  // plain JS objects that are actually in the store.
+  // The toRefArray() method will give us an array of the
+  // plain JS objects for each item in the QuerySet.
+  const pilots = Pilot.all().toRefArray();
 
-        // Use the first pilot as the "current" one for display, if available.
-        const currentPilot = pilots[0] || {};
+  // Now that we have an array of all pilot objects, return it as a prop
+  return { pilots };
+};
 
-        return (
+export class Pilots extends Component {
+  render() {
+    const { pilots = [] } = this.props;
+
+    // Use the first pilot as the "current" one for display, if available.
+    const currentPilot = pilots[0] || {};
+
+    return (
+      <Segment>
+        <Grid>
+          <Grid.Column width={10}>
+            <Header as="h3">Pilot List</Header>
+            <PilotsList pilots={pilots} />
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Header as="h3">Pilot Details</Header>
             <Segment>
-                <Grid>
-                    <Grid.Column width={10}>
-                        <Header as="h3">Pilot List</Header>
-                        <PilotsList pilots={pilots} />
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <Header as="h3">Pilot Details</Header>
-                        <Segment >
-                            <PilotDetails pilot={currentPilot} />
-                        </Segment>
-                    </Grid.Column>
-                </Grid>
+              <PilotDetails pilot={currentPilot} />
             </Segment>
-        );
-    }
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    );
+  }
 }
 
-export default Pilots
+export default connect(mapState)(Pilots);
